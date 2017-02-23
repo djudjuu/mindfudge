4ff7e188d2cfbaff550887a35f5dc53e27f325bc
djudjuu <djuju@thinki.org>
2017-02-23 12:14:53 +0100

    comments

--- mindfudge.sol
+++ mindfudge.sol
@@ -21,23 +21,26 @@ contract Mindfudge  {
       cards1 = [true, true, true, true, true];
       cards2 = [true, true, true ,true, true];
     }
-    
+
+    //put a card that has not been played in the middle
     function playACard(uint card){
-        //errors:
         //which player?
         uint playerindex;
         if (msg.sender == player1){
-            playerindex = 0;
-            if (cards1[card] && middle[0] == 0){
+          playerindex = 0;
+          //check whether has not played the card before AND
+          // whether he has not played in this round before
+          if (cards1[card] && middle[0] == 0){
                 middle[0] = card;
                 cards1[card] = false;
             }else{
                 // Exception : card already played
             }
-            
         }else{
             if (msg.sender == player2){
                 playerindex = 1;
+                //check whether has not played the card before AND
+                // whether he has not played in this round before
                 if(cards2[card] && middle[1] == 0){
                     middle[1] = card;
                     cards1[card] = false;
@@ -46,31 +49,39 @@ contract Mindfudge  {
                 }
             }
         }
+        //both players have submitted a card?
         if (middle[0] != 0 && middle[1] !=0) {
             reveal();
         }
     }
+
+    //find out whos card is higher and assign wins
     function reveal(){
-        if ( middle[0] == middle[1] ) {
+      //DRAW: no one gets a point, but next round is for one more
+      if ( middle[0] == middle[1] ) {
             drawpot += 1;
-        }
-        if ( middle[0]>middle[1] ) {
+      }
+      //player1 wins
+      if ( middle[0]>middle[1] ) {
             wins1 += 1 + drawpot; 
             drawpot = 0;
             if ( wins1 > 2 ){
                 endGame(1);
             }
-        }
-        if ( middle[1]>middle[0] ){ 
+      }
+      //player2 wins
+      if ( middle[1]>middle[0] ){ 
             wins2 += 1  + drawpot;
             drawpot =0;
             if ( wins2 > 2 ){
                 endGame(2);
             }
-        }
-        middle = [0,0];
+      }
+      middle = [0,0];
     }
-    
+
+    //function to declare the game ended
+    //later: payOut Winner
     function endGame(uint winner){
     if (winner == 1){ 
         mindfudger = player1;
@@ -78,7 +89,8 @@ contract Mindfudge  {
         mindfudger = player2; 
         }    
     }
-    
+
+    //queryfunction to find out whether you won
     function didIWin() returns (bool) {
         return msg.sender == mindfudger;
     }
