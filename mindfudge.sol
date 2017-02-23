fea32ef8599966859d5291724e49f569e31b66c3
djudjuu <djuju@thinki.org>
2017-02-23 20:44:13 +0100

    making it smoother

--- mindfudge.sol
+++ mindfudge.sol
@@ -1,57 +1,61 @@
 pragma solidity ^0.4.0;
 /// @title Playing a Mindfudge Game
 contract Mindfudge  {
-    // This declares a new complex type which will
-    // be used for variables later.
-    //uint lenGame = 5;
-    address player1; 
-    address player2;
-    bool[] cards1;
-    bool[] cards2;
-    uint[2] middle = [0,0];
-    uint wins1 = 0;
-    uint wins2 = 0;
-    uint drawpot = 0;
-    address mindfudger;
-    //uint amount
+    // This declares a new complex type for a Player
+  struct Player
+  {
+    address addr;
+    bool[] cards;
+    uint playeridx;
+    uint wins;
+  }
+  Player[2] players;
+  uint[2] middle = [0,0];
+  uint drawpot = 0;
+  address mindfudger;
+  //later: 
+  //uint amount
+  //uint lenGame = 5;
     
-    function Mindfudge(address enemy){
-      player1 = msg.sender;
-      player2 = enemy;
-      cards1 = [true, true, true, true, true];
-      cards2 = [true, true, true ,true, true];
-    }
+  function Mindfudge(address enemy)
+  {
+      players.push(Player({
+              addr: msg.sender,
+              playeridx:0,
+              cards:[true, true, true ,true, true],
+              wins: 0
+              }));
+      players.push(Player({
+              addr: enemy,
+              playeridx:1,
+              cards:[true, true, true ,true, true],
+              wins: 0
+              }));              
+  }
 
-    //put a card that has not been played in the middle
+    //function to put a card that has not been played in the middle
     function playACard(uint card){
-        //which player?
-        uint playerindex;
-        if (msg.sender == player1){
-          playerindex = 0;
-          //check whether has not played the card before AND
-          // whether he has not played in this round before
-          if (cards1[card] && middle[0] == 0){
-                middle[0] = card;
-                cards1[card] = false;
-            }else{
-                // Exception : card already played
-            }
-        }else{
-            if (msg.sender == player2){
-                playerindex = 1;
-                //check whether has not played the card before AND
-                // whether he has not played in this round before
-                if(cards2[card] && middle[1] == 0){
-                    middle[1] = card;
-                    cards1[card] = false;
-                }else{
-                    // Exception : card already played
-                }
-            }
+      //which player sent the card?
+      Player active;
+      if (msg.sender == players[0].addr)
+        {
+        active = players[0];
         }
-        //both players have submitted a card?
-        if (middle[0] != 0 && middle[1] !=0) {
-            reveal();
+      if (msg.sender == players[1].addr)
+        {
+        active = players[1];
+        } else { throw;} //only the two players can play
+      //check whether the card has not been played  before AND
+      // whether the player has not played in this round before
+      if (active.cards[card] && middle[active.playeridx] == 0)
+        {
+          active.cards[card] = false;
+          middle[0] = card;
+        }
+      //both players have submitted a card?
+      if (middle[0] != 0 && middle[1] !=0)
+        {
+        reveal();
         }
     }
 
@@ -61,38 +65,40 @@ contract Mindfudge  {
       if ( middle[0] == middle[1] ) {
             drawpot += 1;
       }
+      uint winneridx;
       //player1 wins
-      if ( middle[0]>middle[1] ) {
-            wins1 += 1 + drawpot; 
-            drawpot = 0;
-            if ( wins1 > 2 ){
-                endGame(1);
-            }
+      if ( middle[0]>middle[1] )
+        {
+        winneridx = 0;
+        } else {
+        winneridx = 1;
       }
-      //player2 wins
-      if ( middle[1]>middle[0] ){ 
-            wins2 += 1  + drawpot;
-            drawpot =0;
-            if ( wins2 > 2 ){
-                endGame(2);
-            }
-      }
+      // augment winners score
+      players[winneridx].wins += 1 + drawpot;
+      // and reset possible extrapoints
+      drawpot = 0;
+      
+      //check if one player has more than half of the cards
+      if ( players[winneridx].wins > 2 )
+        {
+        endGame(winneridx);
+        }
+      //reset middle
       middle = [0,0];
     }
 
     //function to declare the game ended
     //later: payOut Winner
-    function endGame(uint winner){
-    if (winner == 1){ 
-        mindfudger = player1;
-    }else{ 
-        mindfudger = player2; 
-        }    
+    function endGame(uint winner)
+    {
+      mindfudger = players[winner];
+    //end Game and payout everything to the Winner
+    //suicide(mindfudger);
     }
 
     //queryfunction to find out whether you won
     function didIWin() returns (bool) {
         return msg.sender == mindfudger;
-    }
+    } 
 
 }
